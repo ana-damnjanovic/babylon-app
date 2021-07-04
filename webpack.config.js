e352454a1fe32bcaaea69c5a5c277c0b7fb195eb
Ana <ana.damnjanovic@mail.utoronto.ca>
2021-07-03 23:01:22 -0400

    update build to inject html

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,33 @@
 const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const appDirectory = fs.realpathSync(process.cwd());
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: path.resolve(appDirectory, "src/index.ts"),
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
     resolve: {
-        extensions: [".ts", ".js"],
+        extensions: [".tsx", ".ts", ".js"],
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 loader: "ts-loader",
+                exclude: /node_modules/
             },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: path.resolve(appDirectory, "dist/index.html"),
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    mode: "development",
 };
