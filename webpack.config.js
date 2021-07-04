8e347b9cc7f4c988cc15b3c0bdfc5038945cfc06
Ana <ana.damnjanovic@mail.utoronto.ca>
2021-07-04 01:04:49 -0400

    update deploy command and gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: "./src/index.ts",
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        //path: path.resolve(__dirname, 'dist')
     },
     resolve: {
         extensions: [".tsx", ".ts", ".js"],
